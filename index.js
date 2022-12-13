c72b08d4d5f8f5e72ad4097d6adeef68e8b93171
natnaelLake <bewuketulake1212@gmail.com>
2022-12-13 12:37:26 +0300

    the modified commit of course era.

--- index.js
+++ index.js
@@ -6,27 +6,28 @@ const {MongoClient} = require('mongodb');const router = express.Router();
 var url = "mongodb://localhost:27017/";
 const dbName = 'natnael';
 const userRouter = require('./Router/users')
-MongoClient.connect(url, (err, client) => {
-    assert.equal(err, null);
+MongoClient.connect(url).then((client) => {
     console.log('Connected to the database');
     const db = client.db(dbName);
-    dboper.insertDocument(db, { name: "Bewuketu", last: "lake", email: "natilake12@gmail.com", password: "welcomewel" }, "nati", (result) => {
+    dboper.insertDocument(db, { name: "Bewuketu", last: "lake", email: "natilake12@gmail.com", password: "welcomewel" }, "nati").then((result) => {
         console.log("Insert Document:\n", result.ops);
 
-        dboper.findDocuments(db, 'nati', (docs) => {
-            console.log('Found Documents:\n', docs);
-            dboper.updateDocument(db, { name: 'Bewuketu' }, { email: 'updatedemail@gmail.com' }, 'nati',(docs) => {
-                console.log("The updated data is : \n", docs);
-            })
-        })
-        dboper.findDocuments(db, 'nati', (docs) => {
-            console.log('Found Documents:\n', docs);
-            db.dropCollection('nati',(result) => {
-                console.log('the dropped collection is: ',result);
-            })
-        })
-       
+        return dboper.findDocuments(db, 'nati')
+    }).then((docs) => {
+        console.log('Found Documents:\n', docs);
+        return dboper.updateDocument(db, { name: 'Bewuketu' }, { email: 'updatedemail@gmail.com' }, 'nati')
+    }).then((result) => {
+        console.log("The updated data is : \n", result);
+        return dboper.findDocuments(db, 'nati')
+    }).then((result) => {
+        console.log('Found Documents:\n', result);
+        return db.dropCollection('nati')
+    }).then((result) => {
+        console.log('the dropped collection is: ', result);
+        client.close();
     })
+}).catch((err) => {
+    console.log(err);
 })
 app.listen(3000, () => {
     console.log("the server running on: http://localhost:3000")
